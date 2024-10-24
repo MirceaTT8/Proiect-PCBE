37b650e3582595d8e02b5a8e553c4e995d1f3e80
MirceaTT8 <mircea.munteanu356@gmail.com>
2024-10-24 19:07:24 +0300

    first commit

---
+++ src/test/java/com/javazerozahar/stock_exchange/StockExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javazerozahar.stock_exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

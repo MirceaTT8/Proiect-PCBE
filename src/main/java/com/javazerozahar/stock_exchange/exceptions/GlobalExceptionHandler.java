ce660a488a6f917f683f8ac9cc9cd9900a6aedd5
MurariuMarius <mam.murariu@gmail.com>
2024-11-27 20:11:11 +0200

    Added global exception handler

---
+++ src/main/java/com/javazerozahar/stock_exchange/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.javazerozahar.stock_exchange.exceptions;
+
+import com.javazerozahar.stock_exchange.model.dto.ErrorResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Log4j2
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(PortfolioNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handlePortfolioNotFoundException(PortfolioNotFoundException ex) {
+        log.error("PortfolioNotFoundException: {}", ex.getMessage(), ex);
+        ErrorResponse errorResponse = new ErrorResponse("Portfolio Not Found", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleOrderNotFoundException(OrderNotFoundException ex) {
+        log.error("OrderNotFoundException: {}", "The specified order could not be found", ex);
+        ErrorResponse errorResponse = new ErrorResponse("Order Not Found", "The specified order could not be found");
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InsufficientFundsException.class)
+    public ResponseEntity<ErrorResponse> handleInsufficientFundsException(InsufficientFundsException ex) {
+        log.error("InsufficientFundsException: {}", ex.getMessage(), ex);
+        ErrorResponse errorResponse = new ErrorResponse("Insufficient Funds", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CannotPatchException.class)
+    public ResponseEntity<ErrorResponse> handleCannotPatchException(CannotPatchException ex) {
+        log.error("CannotPatchException: {}", ex.getMessage(), ex);
+        ErrorResponse errorResponse = new ErrorResponse("Cannot Patch", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        log.error("Unhandled exception occurred: {}", ex.getMessage(), ex);
+        ErrorResponse errorResponse = new ErrorResponse("Internal Server Error", "An unexpected error occurred");
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}

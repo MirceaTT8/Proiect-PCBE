74abd42b8f265992fc60f5916a96b5464cf70fc3
MurariuMarius <mam.murariu@gmail.com>
2024-11-28 12:45:35 +0200

    Fixed DTO initialisation

--- src/main/java/com/javazerozahar/stock_exchange/model/dto/UserDTO.java
+++ src/main/java/com/javazerozahar/stock_exchange/model/dto/UserDTO.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -19,5 +20,5 @@ public class UserDTO {
     @Updatable
     private String name;
 
-    private List<PortfolioDTO> portfolioDTOs;
+    private List<PortfolioDTO> portfolioDTOs = new ArrayList<>();
 }

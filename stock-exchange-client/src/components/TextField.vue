8f38538a81930d10d15d423952457c3f9e0c0592
Dani Munteanu <daniel.munteanu@student.upt.ro>
2024-11-30 03:12:28 +0200

    Created TextField.vue component

---
+++ stock-exchange-client/src/components/TextField.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="label-container">
+        <label>{{ labelTitle }}</label>
+        <div v-if="isSelected" id="default-value">{{ defaultValue }}</div>
+    </div>
+    <input type="text" :value="inputtedValue" @input="handleInput" required @focus="toggleSelect" @blur="toggleSelect" placeholder="Enter text here...">
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { defineProps } from 'vue';
+
+defineProps({
+    labelTitle: {
+        type: String,
+        default: 'Undefined label'
+    },
+    defaultValue: {
+        type: String,
+        default: 'Undefined default value'
+    },
+    inputtedValue: {
+        type: String,
+        default: ''
+    }
+});
+
+const isSelected = ref(false);
+const toggleSelect = () => {
+    isSelected.value = !isSelected.value;
+}
+const handleInput = (event) => {
+    this.$emit('update', event.target.value);
+}
+
+</script>
+
+<style scoped>
+    label {
+        color: black;
+        display: inline-block;
+        margin-left: 15px;
+        font-weight: bold;
+        max-width: 230px;
+        text-transform: capitalize;
+        font-size: large;
+    }
+    input {
+        display: block;
+        padding: 10px 6px;
+        width: 100%;
+        box-sizing: border-box;
+        border: 3px solid green;
+        border-radius: 15px;
+        color: #555;
+        outline: none;
+        max-width: 500px;
+    }
+    input:focus {
+        border: 3px solid lightgreen
+    }
+    .label-container {
+        display: grid;
+        grid-template-columns: 1fr 0.6fr;
+        grid-gap: 10px;
+        max-width: 500px;
+        font-family: monospace;
+    }
+    #default-value {
+        max-width: 230px;
+        text-align: end;
+        margin-right: 15px;
+    }
+    
+</style>

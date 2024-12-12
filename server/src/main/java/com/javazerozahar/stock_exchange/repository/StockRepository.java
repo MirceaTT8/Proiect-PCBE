a909c5eb4df6df23e5898ab2acc609bdb4e40479
MurariuMarius <mam.murariu@gmail.com>
2024-12-12 03:40:04 +0200

    Filter active stocks when retrieving stock data

--- server/src/main/java/com/javazerozahar/stock_exchange/repository/StockRepository.java
+++ server/src/main/java/com/javazerozahar/stock_exchange/repository/StockRepository.java
@@ -1,7 +1,20 @@
 package com.javazerozahar.stock_exchange.repository;
 
 import com.javazerozahar.stock_exchange.model.entity.Stock;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    @NonNull
+    @Query("SELECT s FROM Stock s WHERE s.id = :stockId AND s.isActive = true")
+    Optional<Stock> findById(@NonNull Long stockId);
+
+    @NonNull
+    @Query("SELECT s FROM Stock s WHERE s.isActive = true")
+    List<Stock> findAll();
 }

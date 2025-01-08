7778d7eab1b0dba105e3c8e5c18e787d1387f9cd
Dani Munteanu <daniel.munteanu@student.upt.ro>
2025-01-08 13:42:14 +0200

    Fixed updateUI() timing on server-configurator

--- server-configurator/src/views/OrderView.vue
+++ server-configurator/src/views/OrderView.vue
@@ -155,6 +155,15 @@ const stopListening = () => {
 }
 
 const updateUI = async (data) => {
+
+  const DEF_DELAY = 1000;
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms || DEF_DELAY));
+  }
+
+  await sleep(100);
+
   const fetchedOrders = await fetchAllOrders();
   orders.value = await attachStockData(fetchedOrders);
   loadedOrders.value = true;

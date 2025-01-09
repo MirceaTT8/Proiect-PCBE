9c1bee300de27a37fddc4dccabc0d523180738e7
MurariuMarius <mam.murariu@gmail.com>
2025-01-09 13:26:56 +0200

    Added PATCH method to CORS filter

--- server-configurator/src/configs/config.js
+++ server-configurator/src/configs/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://4.239.240.103:8443';
+const BASE_URL = 'https://4.239.240.103:8443';
 
 export {
     BASE_URL,
--- server/src/main/java/com/javazerozahar/stock_exchange/utils/SecurityConfig.java
+++ server/src/main/java/com/javazerozahar/stock_exchange/utils/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(false);
         configuration.setMaxAge(3600L);
